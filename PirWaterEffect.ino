0ab41a5a481ad93133144eba51e71c351561f778
Tim Gremalm <tim@gremalm.se>
2017-11-24 15:11:25 +0100

    Pir detection, change state, fade in/out

--- PirWaterEffect.ino
+++ PirWaterEffect.ino
@@ -2,16 +2,92 @@ const int pinW = 3;
 const int pinR = 5;
 const int pinG = 6;
 const int pinB = 9;
+const int pinPir = 12;
 
-int valueSensorPir = 0; 
+int valueSensorPir = 0;
+enum WaterState {
+	Idle,
+	FadeIn,
+	On,
+	FadeOut
+};
+WaterState lastState = Idle;
+WaterState currentState = Idle;
+int brightness = 0;
+
+String waterStateToString(WaterState in) {
+	switch (in) {
+		case Idle:
+			return("Idle");
+			break;
+		case FadeIn:
+			return("FadeIn");
+			break;
+		case On:
+			return("On");
+			break;
+		case FadeOut:
+			return("FadeOut");
+			break;
+	}
+}
+
+void checkStates() {
+	valueSensorPir = digitalRead(pinPir);
+	switch (lastState) {
+		case Idle:
+			if (valueSensorPir == HIGH) {
+				currentState = FadeIn;
+			}
+			break;
+		case FadeIn:
+			if (brightness > 10) {
+				currentState = On;
+			}
+			break;
+		case On:
+			if (valueSensorPir == LOW) {
+				currentState = FadeOut;
+			}
+			break;
+		case FadeOut:
+			if (brightness <= 0) {
+				currentState = Idle;
+			}
+			break;
+	}
+	if (lastState != currentState) {
+		Serial.println(waterStateToString(currentState));
+	}
+	lastState = currentState;
+}
+
+void actStages() {
+	switch (lastState) {
+		case Idle:
+			break;
+		case FadeIn:
+			brightness += 1;
+      Serial.println(brightness);
+			break;
+		case On:
+			break;
+		case FadeOut:
+			brightness -= 1;
+      Serial.println(brightness);
+			break;
+	}
+}
 
 void setup() {
 	Serial.begin(9600);
+	pinMode(pinPir, INPUT);
 }
 
 void loop() {
-	analogWrite(analogOutPin, outputValue);
-	Serial.println(valueSensorPir);
+	checkStates();
+	actStages();
+	//analogWrite(pinW, brightness);
 
-	delay(2);
+	delay(100);
 }
